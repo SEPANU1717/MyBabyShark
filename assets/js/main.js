bb3809b95dbeca268d7759625cd879b745bc2df5
MARK CHRISTIANIEL MANALO <130642137+SEPANU1717@users.noreply.github.com>
2025-02-13 16:48:08 -1200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,22 +11,11 @@ document.getElementById("showFlowersBtn").addEventListener("click", function () 
     document.getElementById("showFlowers").addEventListener("click", function() {
     document.querySelector(".flowers").style.display = "block";
 });
-$(document).ready(function () {
-    $('.container').mouseenter(function () {
-        $('.card').stop().animate({
-            top: '-90px'
-        }, 'slow');
-    }).mouseleave(function () {
-        $('.card').stop().animate({
-            top: 0
-        }, 'slow');
-    });
-});
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var music = document.getElementById("bg-music");
+
     music.play().catch(() => {
-        // If autoplay is blocked, play music on first user interaction
-        document.addEventListener("click", function() {
+        document.addEventListener("click", function () {
             music.play();
         }, { once: true });
     });
